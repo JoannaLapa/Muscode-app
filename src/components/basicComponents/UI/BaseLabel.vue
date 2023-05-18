8fd4ea6c5f8cb0d10758695bf2333bcaf249abaa
Joanna <jlapa.transport@gmail.com>
2023-05-18 11:17:39 +0200

    laybel layout & styles fix

--- src/components/basicComponents/UI/BaseLabel.vue
+++ src/components/basicComponents/UI/BaseLabel.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="absolute bg-black text-white text-center font-medium text-xxs rotate-45 left-[40%] top-[8%] w-[100%] pb-0.5">
-    <p>{{ sale }}</p>
+  <div
+    class="absolute w-[114px] h-[88px] top-0 right-0"
+  >
+    <div class="relative bg-black text-white text-center font-medium text-xxs rotate-45 left-[4px] top-[24px] w-[140px] pb-0.5">
+      <p>{{ sale }}</p>
+    </div>
   </div>
 </template>
 
@@ -15,8 +19,8 @@ const props = defineProps({
 })
 
 const sale = computed(() => {
-  if (!props.element.sale) return;
+  if (!props.element.sale) return
   const countedSale = Math.round(100 - (props.element.sale / props.element.price) * 100)
-  return `-${countedSale}%`;
+  return `-${countedSale}%`
 })
 </script>
