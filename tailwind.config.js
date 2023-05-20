8811fe7c98280574b4b464eced156464700319bb
Joanna <jlapa.transport@gmail.com>
2023-05-20 21:04:03 +0200

    custom styles add

--- tailwind.config.js
+++ tailwind.config.js
@@ -41,10 +41,12 @@ export default {
         'primary-100': '#384a5c', //font color
         'primary-200': '#333', //title color
         'success-100': '#862583', //color when focus
+        'success-200': '#333c85',
         'danger-100': '#9277FF',
         'danger-200': '#D73737',
         'neutral-100': ' #f5f8fa', //hover color
-        'neutral-200': '#b2b2b2' //color borders
+        'neutral-200': '#b2b2b2', //color borders
+        'neutral-300': '#00000080' // black with opacity 0.5
       },
 
       gridTemplateColumns: {
@@ -52,13 +54,28 @@ export default {
       },
 
       maxWidth: {
-        96: '24rem' //384px
+        96: '24rem', //384px
+        203.5: '50.875rem', //814px
+        295: '73.75rem', //1180px
+      },
+
+      minHeight: {
+        2.5: '0.625rem' //10px
       },
 
       spacing: {
+        0.2: '0.0625rem', //1px
         0.5: '0.125rem', //2px
+        0.7: '0.1875rem', //3px
         1.5: '0.375rem', //6px
-        2.5: '0.625rem' //10px
+        2.5: '0.625rem', //10px
+        2.7: '0.6875rem', //11px
+        3.5: '0.875rem', //14px
+        3.7: '0.9375rem', //15px
+        4.3: '1.0625rem', //17px
+        50: '12.5rem', //200px
+        295: '73.75rem', //1180px
+        '68.7': '68.7%',
       },
 
       textUnderlineOffset: {
@@ -70,9 +87,10 @@ export default {
         '3/25': '11.5%',
         '4/25': '16.5%',
         '5/25': '20.5%',
-        '11/25': '43.5%'
+        '11/25': '43.5%',
+        '31.3': '31.3%',
       }
     }
   },
-  plugins: []
+  plugins: [],
 }
